a26049b8bfa0a453d00948a2f97ab292aabbcdf7
proyecto <proyecto@192.168.1.101>
2013-09-09 23:42:55 -0300

    Aca tenes que escribir SI o SI una descripcion de tus cambios asi sabemos que se modifico

--- ProyectoFC/app/Model/Pregunta.php
+++ ProyectoFC/app/Model/Pregunta.php
@@ -3,9 +3,9 @@ class Pregunta extends AppModel {
 	var $name = 'Pregunta';
 	var $validate = array ();
 	// RELACION PREGUNTA: PREGS_CIERRE 1:N
-	var $hasMany = array(
-			'Preg_cierre' => array(
-					'className'     => 'Preg_cierre',
+	var $belongsTo = array(
+			'PregCierre' => array(
+					'className'     => 'PregCierre',
 					'foreignKey'    => 'preg_cierre_id',
 							)
                     	);
